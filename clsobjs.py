b3c5b8c580602c03fb7086c3485ce783dd31224e
KimtVak8143 <mk8143@srmist.edu.in>
2021-09-01 20:54:04 +0530

    added student class

---
+++ clsobjs.py
@@ -0,0 +1,6 @@
+from Student import Student
+
+student1 = Student("Jim", "CSE", "4.2", False)
+student2 = Student("Kum", "ME", "3", False)
+student3 = Student("PAm", "ECE", "5.8", False)
+print(student2.major)
